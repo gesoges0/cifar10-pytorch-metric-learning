e16ba83aa341740df62813b0fa00e861831ed703
elasticnet <elasticnet1995@gmail.com>
2019-11-10 04:26:38 +0900

    [add] EmbeddingNet class

--- network.py
+++ network.py
@@ -1,1 +1,36 @@
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EmbeddingNet(nn.Module):
+    """ 
+        Basic Embedding Network
+    """
+    def __init__(self, fc_input_size=64*6*6):
+        super(EmbeddingNet, self).__init__()
+        self.convnet = nn.Sequential(nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=0),
+                                    nn.ReLU(inline=True),
+                                    nn.MaxPool2d(kernel_size=2, stride=2),
+                                    nn.Conv2d(32, 64, 3),
+                                    nn.ReLU(inline=True),
+                                    nn.MaxPool2d(kernel_size=2, stride=2),
+                                    )
+        self.fc = nn.Sequential(nn.Linear(fc_input_size, 256),
+                                nn.ReLU(inline=True),
+                                nn.Linear(256, 64),
+                                )
+    
+    def forward(self, x):
+        """ convnet -> flatten -> fc 
+            input: torch.Size([batch_size, 3, 64, 64])
+            output: torch.Size([batch_size, 64])
+        """
+        x = self.convnet(x)
+        batch_size = x.size(0)
+        x = x.view(batch_size, -1) # flatten
+        x = self.fc(x)
+        return x
+
+
+
+    
